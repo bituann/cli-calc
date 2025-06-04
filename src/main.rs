94af2d534ce8d1c18da01fe83fbeb57aa94b22f2
bituan <113036994+bituann@users.noreply.github.com>
2025-06-04 09:25:04 +0100

    remove unnecessary comments & slight adjustments

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,9 @@ use std::io;
 
 fn main() {
 	println!("A calculator");
-	println!("Enter a sequence of calculations witha space after every element e.g 4 + 2 - 3");
-	println!("Supported operations: +, -, *, /");
+	println!("Enter an algebraic expression");
+	println!("Supported operations: +, -, *, /\n");
+	println!("Enter the algebraic expression: ");
 	
 	//read input string
 	let mut calc_string = String::new();
@@ -13,106 +14,7 @@ fn main() {
 		.read_line(&mut calc_string)
 		.expect("Cannot read input");
 		
-	println!("{}\n", evaluate(&calc_string, '+'));
-		
-	let mult_tokens: Vec<&str> = calc_string.trim()
-		.split('*').collect();
-		
-	println!("{:?}", mult_tokens);
-	
-	let mut result = 0.0;
-	
-	for mult_token in mult_tokens {
-		//handle division
-		let div_tokens: Vec<&str> = mult_token.trim()
-			.split('/').collect();
-	}
-	
-	
-	/*
-	//store result
-	let mut result: f64 = 0.0;
-	
-	//evaluate expression
-	let length = tokens.len();
-	let mut i = 0;
-	
-	//order of operations taken into account
-	
-	//basic calculation alone, to be removed
-	while i < length {
-		//check for operation
-		match tokens[i] {
-			"+" => {
-				let mut num1: f64 = 0.0;
-				if result == 0.0 {
-					num1 = tokens[i-1].parse()
-						.expect("Not a number");
-				} else {
-					num1 = result;
-				}
-				
-				let num2: f64 = tokens[i+1].parse()
-					.expect("Not a number");
-					
-				result = add(num1, num2);
-				println!("{}", result);
-			}
-			
-			"-" => {
-				let mut num1: f64 = 0.0;
-				if result == 0.0 {
-					num1 = tokens[i-1].parse()
-						.expect("Not a number");
-				} else {
-					num1 = result;
-				}
-				
-				let num2 = tokens[i+1].parse()
-					.expect("Not a number");
-				
-				result = subtract(num1, num2);
-				println!("{}", result);
-			}
-			
-			"*" => {
-				let mut num1: f64;
-				if result == 0.0 {
-					num1 = tokens[i-1].parse()
-						.expect("Not a number");
-				} else {
-					num1 = result;
-				}
-				
-				let num2: f64 = tokens[i+1].parse()
-					.expect("Not a number");
-				
-				result = multiply(num1, num2);
-				println!("{}", result);
-			}
-			
-			"/" => {
-				let mut num1: f64;
-				if result == 0.0 {
-					num1 = tokens[i-1].parse()
-						.expect("Not a number");
-				} else {
-					num1 = result;
-				}
-				
-				let num2: f64 = tokens[i+1].parse()
-					.expect("Not a number");
-				
-				result = divide(num1, num2);
-				println!("{}", result);
-			}
-			
-			&_ => println!(""),
-		}
-		
-		i += 1;
-	}
-	*/
+	println!("\nThe answer is: {}\n", evaluate(&calc_string, '+'));
 }
 
 fn evaluate (expression: &String, operation: char) -> f64 {
